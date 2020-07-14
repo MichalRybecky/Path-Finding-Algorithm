db3a83b6d3381ba7ef2716b3c643419fe376cd6a
Michal Rybecký <56262573+MichalRybecky@users.noreply.github.com>
2020-07-14 17:02:23 +0200

    able to calculate closest A neighbour from B

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import queue
 from tkinter import *
+from operator import attrgetter
 
 root = Tk()
 root.title("Path finding algorithm")
 root.geometry("800x800")
 root.resizable(False,False)
 
-c = Canvas(root, width=800, height=800, background="#E4E4E4")
+WIDTH = 800
+HEIGHT = 800
+
+c = Canvas(root, width=WIDTH, height=HEIGHT, background="#E4E4E4")
 c.pack()
 
 
@@ -19,16 +23,17 @@ Square is 50x50, diagonal is 70,71
 '''
 
 
-class Node:
+class Node(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.g = 0
         self.h = 0
         self.f = 0
+        self.temp_neighbour_list = []
         self.neighbour_list = []
 
-    def get_g_cost(self, x, y):
+    def get_g_cost(self):
         if self.x == a_point_pos[0] and self.y == a_point_pos [1]:
             self.g = 0
         else:
@@ -40,10 +45,10 @@ class Node:
             num_of_x_squares = int(g_cost[0])
             num_of_y_squares = int(g_cost[1])
 
-            self.g = abs((num_of_x_squares - num_of_y_squares) + (((num_of_y_squares - 50) / 50) * 70.71))
-        print(f"G cost: {self.g}")
+            self.g = abs((num_of_x_squares - num_of_y_squares) + (((num_of_y_squares - 50) / 50) * 70.7))
 
-    def get_h_cost(self, x, y):
+    def get_h_cost(self):
+        # Getting node H cost
         if self.x == b_point_pos[0] and self.y == b_point_pos [1]:
             self.h = 0
         else:
@@ -55,12 +60,11 @@ class Node:
             num_of_x_squares = int(h_cost[0])
             num_of_y_squares = int(h_cost[1])
 
-            self.h = abs((num_of_x_squares - num_of_y_squares) + (((num_of_y_squares - 50) / 50) * 70.71))
-        print(f"H cost: {self.h}")
+            self.h = abs((num_of_x_squares - num_of_y_squares) + (((num_of_y_squares - 50) / 50) * 70.7))
 
-    def get_f_cost(self, x, y):
+    def get_f_cost(self):
+        # Getting node F cost
         self.f = self.g + self.h
-        print(f"F cost: {self.f}")
 
     def draw_node(self, x, y, g, h, f):
         # Draw square and G, H and F cost in square
@@ -70,18 +74,67 @@ class Node:
         c.create_text(self.x + 25, self.y + 30, text=int(self.f), font="arial 15 bold")
 
     def get_neighbours(self, x, y):
-        self.neighbour_list.append([self.x - 50, self.y - 50])
-        self.neighbour_list.append([self.x - 50, self.y])
-        self.neighbour_list.append([self.x - 50, self.y + 50])
+        # Adding node neighbours to list
+        self.temp_neighbour_list.append([self.x - 50, self.y - 50])
+        self.temp_neighbour_list.append([self.x - 50, self.y])
+        self.temp_neighbour_list.append([self.x - 50, self.y + 50])
 
-        self.neighbour_list.append([self.x, self.y - 50])
-        self.neighbour_list.append([self.x, self.y + 50])
+        self.temp_neighbour_list.append([self.x, self.y - 50])
+        self.temp_neighbour_list.append([self.x, self.y + 50])
 
-        self.neighbour_list.append([self.x + 50, self.y - 50])
-        self.neighbour_list.append([self.x + 50, self.y])
-        self.neighbour_list.append([self.x + 50, self.y + 50])
+        self.temp_neighbour_list.append([self.x + 50, self.y - 50])
+        self.temp_neighbour_list.append([self.x + 50, self.y])
+        self.temp_neighbour_list.append([self.x + 50, self.y + 50])
 
-        print(self.neighbour_list)
+        nodes_to_delete = []
+        nodes_to_delete.clear()
+
+        # Deleting nodes that are not on the grid
+        for node in self.temp_neighbour_list:
+            if node[0] < 50 or node[0] >= 750 or node[1] < 50 or node[1] >= 750:
+                nodes_to_delete.append(node)
+        for node in nodes_to_delete:
+            self.temp_neighbour_list.remove(node)
+
+        for node in self.temp_neighbour_list:
+            self.neighbour_list.append(Node(node[0], node[1]))
+
+
+    def get_neighbour_data(self, x, y):
+        for node in self.neighbour_list:
+            node.get_g_cost()
+            node.get_h_cost()
+            node.get_f_cost()
+
+    def lowest_f_cost(self):
+        min_f_cost = min(self.neighbour_list, key=attrgetter('f'))
+        return [min_f_cost.x, min_f_cost.y]
+
+
+def main():
+    # There is no A, B or both points on the screen
+    if not a_point_pos or not b_point_pos:
+        print("Missing point(s)")
+    # Main loop
+    else:
+        current = Node(a_point_pos[0], a_point_pos[1])
+        open_l.append(current)
+        while True:
+            current.get_neighbours(current.x, current.y)
+            current.get_neighbour_data(current.x, current.y)
+            current = current.lowest_f_cost()
+            current = Node(current[0], current[1])
+            current.get_g_cost()
+            current.get_h_cost()
+            current.get_f_cost()
+            print(current.f)
+            c.create_rectangle(current.x, current.y, current.x + 50, current.y + 50, fill = "green")
+
+
+
+
+            break
+
 
 
 def mainboard():
@@ -94,7 +147,7 @@ def mainboard():
     b_point_pos.clear()
     walls_pos.clear()
     # Start and Clear Buttons
-    b_clear = Button(c, text="Clear", command=mainboard)
+    b_clear = Button(c, text="Clear", command=clear)
     b_clear.configure(width = 10, relief = FLAT)
     b_clear_window = c.create_window(300, 20, anchor=CENTER, window=b_clear)
     b_start = Button(c, text="Start", command=main)
@@ -102,29 +155,16 @@ def mainboard():
     b_start_window = c.create_window(500, 20, anchor=CENTER, window=b_start)
 
 
-def main():
-    # There is no A, B or both points on the screen
-    if not a_point_pos or not b_point_pos:
-        print("Missing point(s)")
-    else:
-        a_node = Node(a_point_pos[0], a_point_pos[1])
-        b_node = Node(b_point_pos[0], b_point_pos[1])
-        open_l.append([a_node.x, a_node.y])
-        print(open_l)
-        current = a_point_pos
-        list_control = 0
-        while current != b_point_pos:
-            current = open_l[list_control] # TODO: add logic to choose value from list that has the lowest f_cost
-            closed_l.append(current)
-            node = Node(current[0], current[1])
-            print(node.neighbour_list)
-            current_g = node.get_g_cost(current[0], current[1])
-            current_h = node.get_h_cost(current[0], current[1])
-            current_f = node.get_f_cost(current[0], current[1])
-            node.draw_node(current[0], current[1], current_g, current_h, current_f)
-            list_control += 1
-            node.get_neighbours(current[0], current[1])
-            break
+def clear():
+    c.create_rectangle(0, 0, WIDTH, HEIGHT, fill="#E4E4E4")
+    a_point_pos = []
+    b_point_pos = []
+    walls_pos = []
+    open_l = []
+    closed_l = []
+    all_neighbours = []
+    current = []
+    mainboard()
 
 
 def square_clicked(x, y):
@@ -168,10 +208,6 @@ def create_wall(event):
     else:
         c.create_rectangle(x, y, x + 50, y + 50, fill = "grey")
         walls_pos.append([x, y])
-    print(f"A point: {a_point_pos}")
-    print(f"B point: {b_point_pos}")
-    print(f"Walls: {walls_pos}")
-    print("")
 
 
 def create_a(event):
